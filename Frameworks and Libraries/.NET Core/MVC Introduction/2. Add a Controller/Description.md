ed5596660c71680590d88108521c566bebdd3f7c
Enrique M. <epiclife@live.com>
2024-05-18 09:58:18 -0300

    Deleted the full project from the first step, just posting single cs files now

---
+++ Frameworks and Libraries/.NET Core/MVC Introduction/2. Add a Controller/Description.md
@@ -0,0 +1,9 @@
+# 2. Add a Controller to an ASP.NET Core MVC app
+Now, from what I'm seeing at the .NET documentation, it is time to add a custom controller to my project. Of course, there's already controllers created in this project, but the one I created here is called "HelloWorldController".
+
+You see, when you first create a controller in ASP.NET Core. The Controller has a default method already created, called "Index()" which is returning a View method. Now... of course, if you run the project and go to /HelloWorld/ in the browser, you're going to see an error, because the "index" view doesn't exist yet.
+
+Now, in the .NET documentation, they don't tell you this right now, but they tell you to replace the content of this "index" method with some String values. Also, the returning value of the method itself has to change to string.
+
+Changes:
+The only thing you have to know, if that have I created this "HelloWorldController" inside of "Controllers" folder. So, I also think that, I shouldn't submit all the project just for this, so I thought that maybe, submitting this single cs file it works just fine.
---
+++ Frameworks and Libraries/.NET Core/MVC Introduction/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    // It appears that every controller class in ASP.NET Core is inheriting from a "Controller" class.
+    public class HelloWorldController : Controller
+    {
+        /* Default Method that is created with the controller.
+         * It appears to return a method called "View" which makes reference to
+         * a folder and a .cshtml file that should be created inside the "Views" folder
+        */
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        /*
+        Because, obviously, such folder and file doesn't exist yet, then we just comment this
+        and follow the .NET documentation.
+        */
+
+        /*
+         * Here, we replace the method's return type to a string.
+         * Also, there's an annotation that says "/HelloWorld/.
+         * This annotation is telling me that in the browser, if we want to see this
+         * string value, then we should navigate to this URI.
+         */
+
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        /*
+         * Here we have a custom method called "Welcome", it appears that, every single
+         * method other than the "Index" one, you navigate to them including other "/" to the URI.
+         * This of course, is also antoher simple method that just returns a string value.
+         */
+ 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        /*
+         * .NET Documentation talks about these methods as "HTTP Endpoints".
+         * "Every public method in a controller is callable as an HTTP endpoint.
+         * In the sample above, both methods return a string. Note the comments
+         * preceding each method."
+         */
+    }
+}
