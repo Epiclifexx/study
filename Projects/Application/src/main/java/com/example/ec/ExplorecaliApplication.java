1faf978ee2371542fad2f53c1e031d5dad7d3b19
Enrique M. <epiclife@live.com>
2024-05-25 16:20:55 -0300

    Spring files for Microservice project

---
+++ Projects/Application/src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -0,0 +1,14 @@
+package com.example.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// The @SpringBootApplication annotation tells Java that this is where our Microservice starts.
+@SpringBootApplication
+public class ExplorecaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliApplication.class, args);
+	}
+
+}
